dd56bc16cc482b16ced3ec7d5d187f2665e436b3
Kevin Kibe <kchegz234@gmail.com>
2023-05-03 18:51:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,37 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing import image
 
-# Load the model
-model = keras.models.load_model('model_surface_model_2.json')
 
+@st.cache(allow_output_mutation=True)
+def load_model_from_drive():
+    # Authenticate with Google Drive
+    creds = Credentials.from_authorized_user_info(info={
+        'client_id': '1049584246300-rrf03rf4hc4143o6iml77kbg1q37si8a.apps.googleusercontent.com',
+        'client_secret': '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',
+        
+    })
+    service = build('drive', 'v3', credentials=creds)
 
-#Caching the model for faster loading
-@st.cache
+    # Download the model file from Google Drive
+    file_id = 'marble_surface_model_final_1.h5'
+    request = service.files().get_media(fileId=file_id)
+    fh = io.BytesIO()
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        print(f'Download {int(status.progress() * 100)}.')
+    fh.seek(0)
 
+    # Load the model from the downloaded file
+    model = load_model(fh)
+    return model
+
+# Load the model from Google Drive and cache it using st.cache
+model = load_model_from_drive()    
+    
+    
+    
 def predict(image):
     img = keras.preprocessing.image.load_img(image, target_size=(224, 224))
     img_array = keras.preprocessing.image.img_to_array(img)
