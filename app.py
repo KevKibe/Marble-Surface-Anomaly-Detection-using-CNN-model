d933ce4deb2aeff66398e10a9482e352f87bc5aa
Kevin Kibe <kchegz234@gmail.com>
2023-05-18 19:41:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing import image
+from keras.preprocessing.image import img_to_array
+
 from keras.models import load_model
 from PIL import Image
 
